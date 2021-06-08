0efc0d8c61749a38e371c40d85f7dabd7cd7f0c9
Simon Amtoft Pedersen <simon.ipsum@gmail.com>
2021-06-08 16:23:54 +0200

    use class hotdog here

--- project_1_1.py
+++ project_1_1.py
@@ -1,8 +1,10 @@
 import models as m
 import data as d
 from helpers import plt_saliency_img
+import matplotlib.pyplot as plt
 import torch
 import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model_name = "model_1"
@@ -34,18 +36,24 @@ if __name__=="__main__":
     ])
 
     # Load data
-    train_loader, test_loader = d.load_hotdog(train_transform, test_transform, config_1_1)
+    trainset = d.Hotdog_NotHotdog(data_path='./data/hotdog_nothotdog', train=True, transform=train_transform)
+    train_loader = DataLoader(trainset, batch_size=config_1_1["batch_size"], shuffle=True, num_workers=1)
+    testset = d.Hotdog_NotHotdog(data_path='./data/hotdog_nothotdog', train=False, transform=test_transform)
+    test_loader = DataLoader(testset, batch_size=config_1_1["batch_size"], shuffle=False, num_workers=1)
 
-    # Instantiate model
-    model = m.StandardCNN(3, config_1_1).to(device)
-
-    # train model
-    out_dict = m.train(model, config_1_1, project_name, train_loader, test_loader)
-
-    # pick a single image
     images, labels = next(iter(test_loader))
     img = images[23]
 
-    # create and save salience map
-    plt_saliency_img(img, model, save_img=True)
+    # Instantiate model
+    model = m.StandardCNN(config=config_1_1).to(device)
+
+    # # train model
+    out_dict = m.train(model, config_1_1, project_name, train_loader, test_loader)
+
+    # # pick a single image
+    # images, labels = next(iter(test_loader))
+    # img = images[23]
+
+    # # create and save salience map
+    # plt_saliency_img(img, model, save_img=True)
 
