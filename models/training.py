030d70c1ab8aa6cada46603507c195ac27fb8441
Simon Amtoft Pedersen <simon.ipsum@gmail.com>
2021-06-09 15:08:27 +0200

    fix training

--- models/training.py
+++ models/training.py
@@ -1,8 +1,9 @@
 import wandb
-import tqdm
+from tqdm import tqdm
 import numpy as np
 import torch
 import torch.nn as nn
+from torch.utils.data import DataLoader
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -13,45 +14,53 @@ def loss_func(output, target):
     return nn.CrossEntropyLoss()(output, target)
 
 
-def train_2(model, config, project_name, train_loader, test_loader):
-    out_dict = {
-        'train_acc': [],
-        'test_acc': [],
-        'train_loss': [],
-        'test_loss': []
-    }
-
-    # Initialise wandb
-    wandb.init(project=project_name, config=config)
-
-
-def train(model, config, project_name, train_loader, test_loader):
-    out_dict = {
-        'train_acc': [],
-        'test_acc': [],
-        'train_loss': [],
-        'test_loss': []
-    }
-
-    # Initialise wandb
-    wandb.init(project=project_name, config=config)
-    
-    # Set optimizer
+def get_optim(config, model):
+    # set optimizer
     if config["optimizer"] == "adam":
-      optimizer = torch.optim.Adam(model.parameters(), lr=config["learning_rate"])
+        optimizer = torch.optim.Adam(model.parameters(), lr=config["learning_rate"])
     elif config["optimizer"] == "sgd":
-      optimizer = torch.optim.SGD(model.parameters(), lr=config["learning_rate"])
-    
-    # set learning rate scheduler
+        optimizer = torch.optim.SGD(model.parameters(), lr=config["learning_rate"])
+
+    # set scheduler
     if config['step_lr'][0]:
         scheduler = torch.optim.lr_scheduler.StepLR(
             optimizer, 
             step_size=config['step_lr'][1], 
             gamma=config['step_lr'][2]
         )
+    return optimizer, scheduler
+
+
+def get_loaders(config, trainset, testset):
+    train_loader = DataLoader(
+        trainset, batch_size=config["batch_size"], shuffle=True, num_workers=2
+    )
+    test_loader = DataLoader(
+        testset, batch_size=config["batch_size"], shuffle=False, num_workers=2
+    )
+    return train_loader, test_loader
+
+
+def train(model, config, project_name, trainset, testset):
+    out_dict = {
+        'train_acc': [],
+        'test_acc': [],
+        'train_loss': [],
+        'test_loss': []
+    }
+
+    # Initialise wandb
+    wandb.init(project=project_name, config=config)
+    
+    # Set optimizer and acheduler
+    optimizer, scheduler = get_optim(config, model)
+
+    # Get data loaders
+    train_loader, test_loader = get_loaders(config, trainset, testset)
 
     # do training
-    for _ in range(config["epochs"]):
+    print("Start of training\n\n")
+    for _ in tqdm(range(config["epochs"]), desc='epoch'):
         model.train()
         
         # For each epoch
@@ -68,6 +77,7 @@ def train(model, config, project_name, train_loader, test_loader):
             
             # Compute the loss
             loss = loss_func(output, target)
+            train_loss.append(loss.item())
             
             # Backward pass through the network
             loss.backward()
@@ -75,32 +85,30 @@ def train(model, config, project_name, train_loader, test_loader):
             # Update the weights
             optimizer.step()
 
-            train_loss.append(loss.item())
-
-            #Compute how many were correctly classified
-            predicted = output.argmax(1)
-            train_correct.append((target==predicted).sum().cpu().item())
+            # Compute how many were correctly classified
+            predicted = torch.reshape(output, (-1, 11)).argmax(1)
+            train_correct += (target==predicted).sum().cpu().item()
 
         if config['step_lr']:
             scheduler.step()
 
-        #Comput the test accuracy
+        # Compute the test accuracy
         test_loss = []
-        test_correct = []
+        test_correct = 0
         model.eval()
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
             with torch.no_grad():
                 output = model(data)
             test_loss.append(loss_func(output, target).cpu().item())
-            predicted = output.argmax(1)
-            test_correct.append((target==predicted).sum().cpu().item())
+            predicted = torch.reshape(output, (-1, 11)).argmax(1)
+            test_correct += (target==predicted).sum().cpu().item()
 
         # save losses and accuracy
         train_loss = np.mean(train_loss)
         test_loss = np.mean(test_loss)
-        train_acc = np.mean(train_correct)
-        test_acc = np.mean(test_correct)
+        train_acc = train_correct/len(trainset)
+        test_acc = test_correct/len(testset)
         
         # save as dict
         out_dict['train_acc'].append(train_acc)
