6e15e3b713c4bcf432b5ed33853d57361fa75af4
Simon Amtoft Pedersen <simon.ipsum@gmail.com>
2021-06-15 10:31:41 +0200

    clean

--- helpers/performance_metrics.py
+++ helpers/performance_metrics.py
@@ -3,36 +3,33 @@
 import torch
 
 
-def compute_dice(pred, anno):
-    BATCH_SIZE = pred.shape[0]
+def compute_dice(pred, anno, batch_size):
+    # Ensure some stability
+    pred = torch.clamp(torch.sigmoid(pred), 1e-8, 1-1e-8)
     
-    pred2 = torch.sigmoid(pred)
-    num = torch.mean((2 * anno * pred2 + 1).view(BATCH_SIZE,-1),dim=1)
-    den = torch.mean((anno + pred2 + 1).view(BATCH_SIZE,-1),dim=1)
-    DICE =  1 - (num / den)
-    #pred = torch.sigmoid(pred)
-    #num = torch.mean(2 * anno * pred + 1)
-    #den = torch.mean(anno + pred + 1)
-    return DICE
+    # Compute dice
+    num = torch.mean((2 * anno * pred + 1).view(batch_size, -1), dim=1)
+    den = torch.mean((anno + pred + 1).view(batch_size, -1), dim=1)
+    dice = 1 - (num / den)
+    return torch.mean(dice)
 
 
-def compute_iou(pred, anno):
+def compute_iou(pred, anno, batch_size):
     """Computes the intersection over union of the predicted and annotated segmentations"""
-    BATCH_SIZE = pred.shape[0]
-    
+    # Get two areas
     A = pred == 1
     B = anno == 1
-    C = torch.sum((A & B).view(BATCH_SIZE,-1),dim=1)
-    D = torch.sum((A | B).view(BATCH_SIZE,-1),dim=1)
-    nan_idx = D == 0
-
-    IoU = C/D
-    IoU[nan_idx] = 0
     
-    #A = pred == 1
-    #B = anno == 1
-    #return torch.sum(A & B) / torch.sum(A | B)
-    return IoU
+    # Compute IoU
+    C = torch.sum((A & B).view(batch_size, -1), dim=1)
+    D = torch.sum((A | B).view(batch_size, -1), dim=1)
+    IoU = C/D
+    
+    # Ensure stability
+    nan_idx = D == 0
+    IoU[nan_idx] = 0
+
+    return torch.mean(IoU)
 
 
 def compute_accuracy(TP, TN, FP, FN):
@@ -43,7 +40,8 @@ def compute_accuracy(TP, TN, FP, FN):
         FP  (int)   :   Number of false positives predicted
         FN  (int)   :   Number of false negatives predicted
     """
-    return (TP + TN) / (TP + TN + FP + FN)
+    acc = (TP + TN) / (TP + TN + FP + FN)
+    return torch.mean(acc)
 
 
 def compute_sensitivity(TP, FN):
@@ -52,13 +50,14 @@ def compute_sensitivity(TP, FN):
         TP  (int)   :   Number of true positives predicted
         FN  (int)   :   Number of false negatives predicted
     """
-    nan_idx = (TP + FN) == 0
+    # Compute sensitivity 
     sens = TP / (TP + FN)
-    sens[nan_idx] = 0
-    #if (TP + FN) == 0:
-    #    return 0
-    #return TP / (TP + FN)
-    return sens
+
+    # Ensure stability
+    nan_idx = (TP + FN) == 0
+    sens[nan_idx] = 0 
+
+    return torch.mean(sens)
 
 
 def compute_specificity(TN, FP):
@@ -68,38 +67,28 @@ def compute_specificity(TN, FP):
         FP  (int)   :   Number of false positives predicted
         
     """
-    nan_idx = (TN + FP) == 0
+    #  Compute specificity
     spec = TN / (TN + FP)
+
+    # Ensure stability
+    nan_idx = (TN + FP) == 0
     spec[nan_idx] = 0
-    return spec
-    #if (TN + FP) == 0:
-    #    return 0
-    #return TN / (TN + FP)
+
+    return torch.mean(spec)
 
 
 def compute_metrics(pred, anno):
     # Compute confusion metrics
-    BATCH_SIZE = pred.shape[0]
-    TP = torch.sum(((pred == 1) & (anno == 1)).view(BATCH_SIZE,-1),dim=1)
-    TN = torch.sum(((pred == 0) & (anno == 0)).view(BATCH_SIZE,-1),dim=1)
-    FP = torch.sum(((pred == 1) & (anno == 0)).view(BATCH_SIZE,-1),dim=1)
-    FN = torch.sum(((pred == 0) & (anno == 1)).view(BATCH_SIZE,-1),dim=1)
-    #TP = torch.sum((pred == 1) & (anno == 1))
-    #TN = torch.sum((pred == 0) & (anno == 0))
-    #FP = torch.sum((pred == 1) & (anno == 0))
-    #FN = torch.sum((pred == 0) & (anno == 1))
+    batch_size = pred.shape[0]
+    TP = torch.sum(((pred == 1) & (anno == 1)).view(batch_size, -1), dim=1)
+    TN = torch.sum(((pred == 0) & (anno == 0)).view(batch_size, -1), dim=1)
+    FP = torch.sum(((pred == 1) & (anno == 0)).view(batch_size, -1), dim=1)
+    FN = torch.sum(((pred == 0) & (anno == 1)).view(batch_size, -1), dim=1)
     
     # Compute performance metrics
-    dice = compute_dice(pred, anno)
-    iou = compute_iou(pred, anno)
+    dice = compute_dice(pred, anno, batch_size)
+    iou = compute_iou(pred, anno, batch_size)
     acc = compute_accuracy(TP, TN, FP, FN)
     sens = compute_sensitivity(TP, FN)
     spec = compute_specificity(TN, FP)
-    
-    acc = torch.mean(acc)
-    sens = torch.mean(sens)
-    spec = torch.mean(spec)
-    dice = torch.mean(dice)
-    iou = torch.mean(iou)
-    
     return (dice, iou, acc, sens, spec)
