a9c4d689a02e9c636015ec26ab209708c6f7e0fc
Simon Amtoft Pedersen <simon.ipsum@gmail.com>
2021-06-15 09:47:36 +0200

    enable config to decide padding

--- models/UNet.py
+++ models/UNet.py
@@ -4,56 +4,6 @@ import torch.nn.functional as F
 from torchvision.transforms.functional import center_crop
 
 
-class UNetSimple(nn.Module):
-    def __init__(self):
-        super(UNetSimple, self).__init__()
-
-        # encoder (downsampling)
-        self.enc_conv0 = nn.Conv2d(1, 64, 3, padding=1)
-        self.pool0 = nn.Conv2d(64, 64, 2, padding=0, stride=2)
-        self.enc_conv1 = nn.Conv2d(64, 64, 3, padding=1)
-        self.pool1 = nn.Conv2d(64, 64, 2, padding=0, stride=2)
-        self.enc_conv2 = nn.Conv2d(64, 64, 3, padding=1)
-        self.pool2 = nn.Conv2d(64, 64, 2, padding=0, stride=2)
-        self.enc_conv3 = nn.Conv2d(64, 64, 3, padding=1)
-        self.pool3 = nn.Conv2d(64, 64, 2, padding=0, stride=2)
-
-        # bottleneck
-        self.bottleneck_conv = nn.Conv2d(64, 64, 3, padding=1)
-
-        # decoder (upsampling)
-        self.upsample0 = nn.ConvTranspose2d(64, 64, 4, stride=2)
-        self.dec_conv0 = nn.Conv2d(128, 64, 3, padding=1)
-        self.upsample1 = nn.ConvTranspose2d(64, 64, 4, stride=2)
-        self.dec_conv1 = nn.Conv2d(128, 64, 3, padding=1)
-        self.upsample2 = nn.ConvTranspose2d(64, 64, 4, stride=2)
-        self.dec_conv2 = nn.Conv2d(128, 64, 3, padding=1)
-        self.upsample3 = nn.ConvTranspose2d(64, 64, 4, stride=2)
-        self.dec_conv3 = nn.Conv2d(128, 1, 3, padding=1)
-
-    def forward(self, x):
-        # encoder
-        e0 = F.relu(self.enc_conv0(x))
-        e0p = self.pool0(e0)
-        e1 = F.relu(self.enc_conv1(e0p))
-        e1p = self.pool1(e1)
-        e2 = F.relu(self.enc_conv2(e1p))
-        e2p = self.pool2(e2)
-        e3 = F.relu(self.enc_conv3(e2p))
-        e3p = self.pool3(e3)
-
-        # bottleneck
-        b = F.relu(self.bottleneck_conv(e3p))
-
-        # decoder
-        d0 = F.relu(self.dec_conv0(skip_connection(e3, self.upsample0(b))))
-        d1 = F.relu(self.dec_conv1(skip_connection(e2, self.upsample1(d0))))
-        d2 = F.relu(self.dec_conv2(skip_connection(e1, self.upsample2(d1))))
-        d3 = self.dec_conv3(skip_connection(e0, self.upsample3(d2)))  # no activation
-        return d3
-
-
-
 class UNet(nn.Module):
     def __init__(self, config):
         super().__init__()
@@ -73,13 +23,15 @@ class UNet(nn.Module):
 
             self.enc_conv.append(nn.Sequential(*module_list))
             self.enc_pool.append(
-                 nn.Conv2d(enc_dims[i], enc_dims[i], kernel_size=2, padding=0, stride=2)
+                 nn.Conv2d(enc_dims[i], enc_dims[i], kernel_size=2, stride=2)
             )
 
         # bottleneck
         module_list = []
         for _ in range(config['n_convs']-1):
-            module_list.append(nn.Conv2d(enc_dims[i], enc_dims[i], kernel_size=3, padding=1))
+            module_list.append(nn.Conv2d(
+                enc_dims[i], enc_dims[i], kernel_size=3, padding=config['padding']
+            ))
             module_list.append(nn.ReLU())
         self.bottleneck = nn.Sequential(*module_list)
 
@@ -97,9 +49,9 @@ class UNet(nn.Module):
             )
         # final layer is without ReLU activation.
         self.dec_conv.append(nn.Sequential(
-            nn.Conv2d(2*dec_dims[i], dec_dims[i], kernel_size=3, padding=1),
-            nn.Conv2d(dec_dims[i], n_labels, kernel_size=3, padding=1),
-            nn.Conv2d(n_labels, n_labels, kernel_size=1, padding=0)
+            nn.Conv2d(2*dec_dims[i], dec_dims[i], kernel_size=3, padding=config['padding']),
+            nn.Conv2d(dec_dims[i], n_labels, kernel_size=3, padding=config['padding']),
+            nn.Conv2d(n_labels, n_labels, kernel_size=1)
         ))
         self.dec_upsample.append(
             nn.ConvTranspose2d(dec_dims[i], dec_dims[i], kernel_size=4, stride=2)
@@ -148,7 +100,7 @@ def append_layer(module_list, dim_1, dim_2, config):
 
     # Add convolutional layer
     out_list.append(
-        nn.Conv2d(dim_1, dim_2, kernel_size=3, padding=1)
+        nn.Conv2d(dim_1, dim_2, kernel_size=3, padding=config['padding'])
     )
 
     # add batch norm
@@ -165,3 +117,52 @@ def append_layer(module_list, dim_1, dim_2, config):
         out_list.append(nn.Dropout(p=config['dropout']))
     
     return out_list
+
+
+class UNetSimple(nn.Module):
+    def __init__(self):
+        super(UNetSimple, self).__init__()
+
+        # encoder (downsampling)
+        self.enc_conv0 = nn.Conv2d(1, 64, 3, padding=1)
+        self.pool0 = nn.Conv2d(64, 64, 2, padding=0, stride=2)
+        self.enc_conv1 = nn.Conv2d(64, 64, 3, padding=1)
+        self.pool1 = nn.Conv2d(64, 64, 2, padding=0, stride=2)
+        self.enc_conv2 = nn.Conv2d(64, 64, 3, padding=1)
+        self.pool2 = nn.Conv2d(64, 64, 2, padding=0, stride=2)
+        self.enc_conv3 = nn.Conv2d(64, 64, 3, padding=1)
+        self.pool3 = nn.Conv2d(64, 64, 2, padding=0, stride=2)
+
+        # bottleneck
+        self.bottleneck_conv = nn.Conv2d(64, 64, 3, padding=1)
+
+        # decoder (upsampling)
+        self.upsample0 = nn.ConvTranspose2d(64, 64, 4, stride=2)
+        self.dec_conv0 = nn.Conv2d(128, 64, 3, padding=1)
+        self.upsample1 = nn.ConvTranspose2d(64, 64, 4, stride=2)
+        self.dec_conv1 = nn.Conv2d(128, 64, 3, padding=1)
+        self.upsample2 = nn.ConvTranspose2d(64, 64, 4, stride=2)
+        self.dec_conv2 = nn.Conv2d(128, 64, 3, padding=1)
+        self.upsample3 = nn.ConvTranspose2d(64, 64, 4, stride=2)
+        self.dec_conv3 = nn.Conv2d(128, 1, 3, padding=1)
+
+    def forward(self, x):
+        # encoder
+        e0 = F.relu(self.enc_conv0(x))
+        e0p = self.pool0(e0)
+        e1 = F.relu(self.enc_conv1(e0p))
+        e1p = self.pool1(e1)
+        e2 = F.relu(self.enc_conv2(e1p))
+        e2p = self.pool2(e2)
+        e3 = F.relu(self.enc_conv3(e2p))
+        e3p = self.pool3(e3)
+
+        # bottleneck
+        b = F.relu(self.bottleneck_conv(e3p))
+
+        # decoder
+        d0 = F.relu(self.dec_conv0(skip_connection(e3, self.upsample0(b))))
+        d1 = F.relu(self.dec_conv1(skip_connection(e2, self.upsample1(d0))))
+        d2 = F.relu(self.dec_conv2(skip_connection(e1, self.upsample2(d1))))
+        d3 = self.dec_conv3(skip_connection(e0, self.upsample3(d2)))  # no activation
+        return d3
