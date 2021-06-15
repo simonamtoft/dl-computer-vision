070da6834aa1df10522496937eb9b53eec0a9c06
Marc Bøg <s173905@student.dtu.dk>
2021-06-15 15:39:55 +0200

    Calculate GED for ensemble

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -1,4 +1,4 @@
 from .saliency import plt_saliency_img
 from .bounding_boxes import compute_receptive_field, non_maximum_supression, \
     get_iou, plt_bboxes
-from .performance_metrics import compute_metrics
+from .performance_metrics import compute_metrics, compute_iou
