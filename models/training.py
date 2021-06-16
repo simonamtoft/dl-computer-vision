92dac0f781abffae32b6e5ea2718e6d0cc144290
Simon Amtoft Pedersen <simon.ipsum@gmail.com>
2021-06-16 08:58:19 +0200

    Update training.py

--- models/training.py
+++ models/training.py
@@ -222,6 +222,7 @@ def train_medical(model, config, train_loader, val_loader, project_name="tmp", p
                 print('\nTRAINING\n')
                 print(Y_pred.shape, Y_pred.type())
                 print(Y_train.shape, Y_train.type())
+                print(len(train_loader))
                 metrics_train = update_metrics(metrics_train, Y_pred, Y_train, len(train_loader))
         
         # Step the learning rate
@@ -249,6 +250,7 @@ def train_medical(model, config, train_loader, val_loader, project_name="tmp", p
                 print('\nVALIDATION\n')
                 print(Y_pred.shape, Y_pred.type())
                 print(Y_train.shape, Y_train.type())
+                print(len(val_loader))
                 metrics_val = update_metrics(metrics_val, Y_pred, Y_val, len(val_loader))
 
         # Plot annotations against model predictions on validation data
