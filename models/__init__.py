5de1ba34624f4e54d4691bd1f3a313f004ce7ba0
Simon Amtoft Pedersen <simon.ipsum@gmail.com>
2021-06-16 13:28:21 +0200

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,4 @@
 from .CNN import StandardCNN, PureCNN
 from .UNet import UNet, UNetSimple
-from .training import train, train_medical, train_anno_ensemble, train_ensemble
-from .losses import loss_func
+# from .training import train, train_medical, train_anno_ensemble, train_ensemble
+# from .losses import loss_func
