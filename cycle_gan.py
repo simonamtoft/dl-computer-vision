b23cb2d6dddec3e27987e2193e8c63cf7edae87d
Simon Amtoft Pedersen <simon.ipsum@gmail.com>
2021-06-18 11:27:21 +0200

    add train script

---
+++ cycle_gan.py
@@ -0,0 +1,65 @@
+# Project 3: Convert Horses to Zebras with Cycle GANs
+
+import os
+import numpy as np
+import glob
+import time
+import PIL.Image as Image
+from itertools import chain
+from collections import OrderedDict
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+import matplotlib.pylab as plt
+import ipywidgets
+from IPython import display
+
+from data import HORSES, ZEBRAS
+from models import Discriminator, Generator
+from training import train_cycle_gan
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Define name of project on weight and biases site
+project_name = "project-3"
+
+# Set config of training run
+config = {
+    'batch_size': 1,
+    'epochs': 10,
+    'lr_d': 4*1e-4,
+    'lr_g': 1*1e-4,
+    'n_features': 64,
+    'n_blocks': 6,
+    'relu_val': 0.2,
+    'img_loss': 'l2',
+    'g_loss_weight': [1, 10, 5]
+}
+
+# Define data transforms
+transform = transforms.Compose([
+    transforms.Resize((128, 128)), 
+    transforms.ToTensor()
+])
+
+# Load zebra and horse image data
+trainset_z = ZEBRAS(dataset = "train", transform=transform)
+testset_z = ZEBRAS(dataset = "test", transform=transform)
+trainset_h = HORSES(dataset = "train", transform=transform)
+testset_h = HORSES(dataset = "test", transform=transform)
+
+# Setup DataLoaders
+zebra_loader = DataLoader(trainset_z, batch_size=config['batch_size'], shuffle=True)
+horse_loader = DataLoader(trainset_h, batch_size=config['batch_size'], shuffle=True)
+
+# Instantiate Cycle GAN network
+d_h = Discriminator(config)
+d_z = Discriminator(config)
+g_h2z = Generator(config)
+g_z2h = Generator(config)
+
+# Train network
+train_cycle_gan(config, g_h2z, g_z2h, d_h, d_z, zebra_loader, horse_loader, project_name)
