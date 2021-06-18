c7ec69922bfeaaff6da70df12fbbd05bd4bbbc71
simonamtoft <s173936@student.dtu.dk>
2021-06-19 00:23:32 +0200

    renaming of variables st. everything is similar

--- training/training_3.py
+++ training/training_3.py
@@ -18,36 +18,37 @@ def fake_loss(x):
     return torch.mean(x**2)
 
 
-def train_cycle_gan(config, g_h2z, g_z2h, d_h, d_z, zebra_loader, horse_loader, p_name='tmp', plotting=False):
+def train_cycle_gan(config, g_h2z, g_z2h, d_h, d_z, z_dl, h_dl, p_name='tmp', plotting=False):
     """Training function for the Cycle GAN network
     Inputs
-        config          :   A config dict that is sent to weight and biases, 
-                            and used to set different training hyperparameters
-                                'lr_g'          :   Learning rate for the 
-                                                    generator optimizer
-                                'lr_d'          :   Learning rate for the 
-                                                    discriminator optimizer
-                                'epochs'        :   Number of epochs to train
-                                'img_loss'      :   Specify how to compute the 
-                                                    image losses as list of 
-                                                    strings 'l1' or 'l2':
-                                                    [cycele, identity]
-                                'g_loss_weight' :   Specify the weighting of
-                                                    the generator losses as a list 
-                                                    of ints: [fool, cycle, identity]
-                                'buffer_size'   :   Size of the image buffer during 
-                                                    training for the generated zebra
-                                                    and horse images.
-        g_h2z           :   A generator nn.Module that converts horses to zebras
-        g_z2h           :   A generator nn.Module that converts zebras to horses
-        d_h             :   A discriminator nn.Module that can discriminate horses
-        d_z             :   A discriminator nn.Module that can discriminate zebras
-        zebra_loader    :   A Dataloader of the zebra training data
-        horse_loader    :   A Dataloader of the horse training data
-        p_name          :   A string, determining the name of the project on wandb
+        config  :   A config dict that is sent to weight and biases, 
+                    and used to set different training hyperparameters
+                        'lr_g'          :   Learning rate for the 
+                                            generator optimizer
+                        'lr_d'          :   Learning rate for the 
+                                            discriminator optimizer
+                        'epochs'        :   Number of epochs to train
+                        'img_loss'      :   Specify how to compute the 
+                                            image losses as list of 
+                                            strings 'l1' or 'l2':
+                                            [cycele, identity]
+                        'g_loss_weight' :   Specify the weighting of
+                                            the generator losses as a list 
+                                            of ints: [fool, cycle, identity]
+                        'buf_size'      :   Size of the image buffer during 
+                                            training for the generated zebra
+                                            and horse images.
+        g_h2z   :   A generator nn.Module that converts horses to zebras
+        g_z2h   :   A generator nn.Module that converts zebras to horses
+        d_h     :   A discriminator nn.Module that can discriminate horses
+        d_z     :   A discriminator nn.Module that can discriminate zebras
+        z_dl    :   A Dataloader of the zebra training data
+        h_dl    :   A Dataloader of the horse training data
+        p_name  :   A string, determining the name of the project on wandb
     """    
     # Define image losses
     im_loss_1, im_loss_2  = gan_im_loss(config)
+    glw = config['g_loss_weight']
 
     # Initialize wandb run
     wandb.init(project=p_name, config=config)
@@ -58,17 +59,19 @@ def train_cycle_gan(config, g_h2z, g_z2h, d_h, d_z, zebra_loader, horse_loader, 
     g_opt = torch.optim.Adam(g_param, lr=config["lr_g"], betas=(0.5, 0.999))
     d_opt = torch.optim.Adam(d_param, lr=config["lr_d"], betas=(0.5, 0.999))
 
-    if "buffer_size" in config and config["buffer_size"]:
-        fake_h_buffer = ImageBuffer(config["buffer_size"])
-        fake_z_buffer = ImageBuffer(config["buffer_size"])
+    if "buf_size" in config and config["buf_size"]:
+        h_buffer = ImageBuffer(config["buf_size"])
+        z_buffer = ImageBuffer(config["buf_size"])
+    else:
+        print("Notice: Not using buffer!")
 
     # perform training
     for epoch in range(config['epochs']):
         print(f"Epoch {epoch+1}/{config['epochs']}")
 
         # Converte loaders to iterators
-        data_zebra = iter(zebra_loader)
-        data_horse = iter(horse_loader)
+        data_z = iter(z_dl)
+        data_h = iter(h_dl)
 
         # Define logging dict for wandb
         logging = {
@@ -80,76 +83,72 @@ def train_cycle_gan(config, g_h2z, g_z2h, d_h, d_z, zebra_loader, horse_loader, 
         }
 
         # Go over all batches
-        for i in range(len(data_zebra)):
+        n_z = len(data_z)
+        for i in range(n_z):
             # Get zebras
-            x_zebra = next(data_zebra).to(device) * 2 - 1
+            x_z = next(data_z).to(device) * 2 - 1
             
             # Get horses
             # Using hack to keep getting horse images, even though there are less of them
             # https://discuss.pytorch.org/t/infinite-dataloader/17903/10
             try: 
-                x_horse = next(data_horse).to(device) * 2 - 1
+                x_h = next(data_h).to(device) * 2 - 1
             except StopIteration:
-                data_horse = iter(horse_loader)
-                x_horse = next(data_horse).to(device) * 2 - 1
+                data_h = iter(h_dl)
+                x_h = next(data_h).to(device) * 2 - 1
 
             # Generate fake images
-            x_zebra_fake = g_h2z(x_horse)
-            x_horse_fake = g_z2h(x_zebra)
+            x_z_fake = g_h2z(x_h)
+            x_h_fake = g_z2h(x_z)
 
             # Generate recreational images
-            x_zebra_rec = g_h2z(x_horse_fake)
-            x_horse_rec = g_z2h(x_zebra_fake)
+            x_z_rec = g_h2z(x_h_fake)
+            x_h_rec = g_z2h(x_z_fake)
 
-            if "buffer_size" in config and config["buffer_size"]:
-                x_horse_fake_t = fake_h_buffer.push_and_pop(x_horse_fake)
-                x_zebra_fake_t = fake_z_buffer.push_and_pop(x_zebra_fake)
+            if "buf_size" in config and config["buf_size"]:
+                x_h_fake_t = h_buffer.push_and_pop(x_h_fake)
+                x_z_fake_t = z_buffer.push_and_pop(x_z_fake)
             else:
-                x_horse_fake_t = x_horse_fake
-                x_zebra_fake_t = x_zebra_fake
+                x_h_fake_t = x_h_fake
+                x_z_fake_t = x_z_fake
 
             # Update discriminator 
             d_opt.zero_grad()
-            d_loss = real_loss(d_h(x_horse))
-            d_loss += fake_loss(d_h(x_horse_fake_t.detach()))
-            d_loss += real_loss(d_z(x_zebra))
-            d_loss += fake_loss(d_z(x_zebra_fake_t.detach()))
-            d_loss.backward()
+            d_l = real_loss(d_h(x_h))
+            d_l += fake_loss(d_h(x_h_fake_t.detach()))
+            d_l += real_loss(d_z(x_z))
+            d_l += fake_loss(d_z(x_z_fake_t.detach()))
+            d_l.backward()
             d_opt.step()
 
             # Update generator
             g_opt.zero_grad()
-            g_loss_fool = real_loss(d_h(x_horse_fake))
-            g_loss_fool += real_loss(d_z(x_zebra_fake))
-            g_loss_fool *= config['g_loss_weight'][0]
-            g_loss_cycle = im_loss_1(x_horse, x_horse_rec)
-            g_loss_cycle += im_loss_1(x_zebra, x_zebra_rec)
-            g_loss_cycle *= config['g_loss_weight'][1]
-            g_loss_iden = im_loss_2(g_h2z(x_zebra), x_zebra)
-            g_loss_iden += im_loss_2(g_z2h(x_horse), x_horse)
-            g_loss_iden *= config['g_loss_weight'][2]
-            g_loss = g_loss_fool + g_loss_cycle + g_loss_iden
-            g_loss.backward()
+            g_l_fool = (real_loss(d_h(x_h_fake)) + real_loss(d_z(x_z_fake))) * glw[0]
+            g_l_cycle = (im_loss_1(x_h, x_h_rec) + im_loss_1(x_z, x_z_rec)) * glw[1]
+            g_l_iden = (im_loss_2(g_h2z(x_z), x_z) + im_loss_2(g_z2h(x_h), x_h)) * glw[2]
+            g_l = g_l_fool + g_l_cycle + g_l_iden
+            g_l.backward()
             g_opt.step()
 
             # Update batch losses
-            logging['d_loss'] += d_loss.item()/len(data_zebra)
-            logging['g_loss'] += g_loss.item()/len(data_zebra)
-            logging['g_loss_fool'] += g_loss_fool.item()/len(data_zebra)
-            logging['g_loss_cycle'] += g_loss_cycle.item()/len(data_zebra)
-            logging['g_loss_iden'] += g_loss_iden.item()/len(data_zebra)
+            logging['d_loss'] += d_l.item()/n_z
+            logging['g_loss'] += g_l.item()/n_z
+            logging['g_loss_fool'] += g_l_fool.item()/n_z
+            logging['g_loss_cycle'] += g_l_cycle.item()/n_z
+            logging['g_loss_iden'] += g_l_iden.item()/n_z
 
         # Make a visualization each epoch (logged to wandb)
-        visualize_train(config, g_h2z, g_z2h, d_h, d_z, x_horse, x_zebra, plotting)
+        visualize_train(im_loss_1, im_loss_2, g_h2z, g_z2h, d_h, d_z, x_h, x_z, glw, plotting)
         
         # Save state every epoch
         save_state(g_h2z, g_z2h, d_h, d_z)
 
         # Log losses to wandb
-        wandb.log(logging)
+        wandb.log(logging, commit=True)
     
     # Finalize run
     wandb.finish()
+    return None
 
 
 def save_state(g_h2z, g_z2h, d_h, d_z):
@@ -157,80 +156,78 @@ def save_state(g_h2z, g_z2h, d_h, d_z):
     torch.save(g_z2h, path.join(save_folder, 'g_z2h.pt'))
     torch.save(d_h, path.join(save_folder, 'd_h.pt'))
     torch.save(d_z, path.join(save_folder, 'd_z.pt'))
+    return None
 
 
-def visualize_train(config, H2Z, Z2H, d_H, d_Z, x_horse, x_zebra, plotting=False):
-    # Define image loss as L2 loss
-    im_loss_1, im_loss_2 = gan_im_loss(config)
-    lw = config['g_loss_weight']
-
+def visualize_train(im_loss_1, im_loss_2, g_h2z, g_z2h, d_h, d_z, x_h, x_z, glw, plotting=False):
+    # Func to fix images before imshow
     def fix_img(x):
-        return np.swapaxes(np.swapaxes((x.cpu().numpy()+1)/2,0,2),0,1)
+        return np.swapaxes(np.swapaxes((x.cpu().numpy() + 1)/2, 0, 2), 0, 1)
 
     with torch.no_grad(): 
         # Generate fake images
-        Z_fake = H2Z(x_horse)
-        H_fake = Z2H(x_zebra)
+        z_fake = g_h2z(x_h)
+        h_fake = g_z2h(x_z)
 
         # Generate recreational images
-        Z_rec = H2Z(H_fake)
-        H_rec = Z2H(Z_fake)
+        z_rec = g_h2z(h_fake)
+        h_rec = g_z2h(z_fake)
 
         # Generate Identity images
-        Z_iden = H2Z(x_zebra)
-        H_iden = Z2H(x_horse)
+        z_iden = g_h2z(x_z)
+        h_iden = g_z2h(x_h)
         
         # Compute losses
-        Z_fake_loss = lw[0]*fake_loss(d_Z(Z_fake)).cpu().numpy()
-        H_fake_loss = lw[0]*fake_loss(d_H(H_fake)).cpu().numpy()
-        Z_rec_loss = lw[1]*im_loss_1(x_zebra, Z_rec).cpu().numpy()
-        H_rec_loss = lw[1]*im_loss_1(x_horse, H_rec).cpu().numpy()
-        Z_iden_loss = lw[2]*im_loss_2(x_zebra, Z_iden).cpu().numpy()
-        H_iden_loss = lw[2]*im_loss_2(x_horse, H_iden).cpu().numpy()
+        z_fake_loss = glw[0]*fake_loss(d_z(z_fake)).cpu().numpy()
+        h_fake_loss = glw[0]*fake_loss(d_h(h_fake)).cpu().numpy()
+        z_rec_loss = glw[1]*im_loss_1(x_z, z_rec).cpu().numpy()
+        h_rec_loss = glw[1]*im_loss_1(x_h, h_rec).cpu().numpy()
+        z_iden_loss = glw[2]*im_loss_2(x_z, z_iden).cpu().numpy()
+        h_iden_loss = glw[2]*im_loss_2(x_h, h_iden).cpu().numpy()
 
     # Get random indicies from batch
-    n_rows = 1 if x_horse.shape[0] < 2 else 2 # How many rows should be shown
-    if x_horse.shape[0] > x_zebra.shape[0]:
-        idx = np.random.randint(0, x_zebra.shape[0], n_rows)
+    n_rows = 1 if x_h.shape[0] < 2 else 2
+    if x_h.shape[0] > x_z.shape[0]:
+        idx = np.random.randint(0, x_z.shape[0], n_rows)
     else:
-        idx = np.random.randint(0, x_horse.shape[0], n_rows)
+        idx = np.random.randint(0, x_h.shape[0], n_rows)
 
     # Plot images
     f,ax = plt.subplots(n_rows*2, 4, figsize=(8, n_rows*5))
     for i in range(n_rows):
         # Horses
-        ax[2*i,0].imshow(fix_img(x_horse[idx[i]]))
+        ax[2*i,0].imshow(fix_img(x_h[idx[i]]))
         ax[2*i,0].axis('off')
         ax[2*i,0].set_title('Original')
         
-        ax[2*i,1].imshow(fix_img(Z_fake[idx[i]]))
+        ax[2*i,1].imshow(fix_img(z_fake[idx[i]]))
         ax[2*i,1].axis('off')
-        ax[2*i,1].set_title('Fake, d={:.2f}'.format(Z_fake_loss))
+        ax[2*i,1].set_title('Fake, d={:.2f}'.format(z_fake_loss))
     
-        ax[2*i,2].imshow(fix_img(H_rec[idx[i]]))
+        ax[2*i,2].imshow(fix_img(h_rec[idx[i]]))
         ax[2*i,2].axis('off')
-        ax[2*i,2].set_title(f'Recovered, d={np.round(H_rec_loss,2)}')
+        ax[2*i,2].set_title(f'Recovered, d={np.round(h_rec_loss, 2)}')
         
-        ax[2*i,3].imshow(fix_img(H_iden[idx[i]]))
+        ax[2*i,3].imshow(fix_img(h_iden[idx[i]]))
         ax[2*i,3].axis('off')
-        ax[2*i,3].set_title(f'Identity, d={np.round(H_iden_loss,2)}')
+        ax[2*i,3].set_title(f'Identity, d={np.round(h_iden_loss, 2)}')
 
         # Zebras
-        ax[2*i+1,0].imshow(fix_img(x_zebra[idx[i]]))
+        ax[2*i+1,0].imshow(fix_img(x_z[idx[i]]))
         ax[2*i+1,0].axis('off')
         ax[2*i+1,0].set_title('Original')
 
-        ax[2*i+1,1].imshow(fix_img(H_fake[idx[i]]))
+        ax[2*i+1,1].imshow(fix_img(h_fake[idx[i]]))
         ax[2*i+1,1].axis('off')
-        ax[2*i+1,1].set_title('Fake, d={:.2f}'.format(H_fake_loss))
+        ax[2*i+1,1].set_title('Fake, d={:.2f}'.format(h_fake_loss))
 
-        ax[2*i+1,2].imshow(fix_img(Z_rec[idx[i]]))
+        ax[2*i+1,2].imshow(fix_img(z_rec[idx[i]]))
         ax[2*i+1,2].axis('off')
-        ax[2*i+1,2].set_title(f'Recovered, d={np.round(Z_rec_loss,2)}')
+        ax[2*i+1,2].set_title(f'Recovered, d={np.round(z_rec_loss, 2)}')
 
-        ax[2*i+1,3].imshow(fix_img(Z_iden[idx[i]]))
+        ax[2*i+1,3].imshow(fix_img(z_iden[idx[i]]))
         ax[2*i+1,3].axis('off')
-        ax[2*i+1,3].set_title(f'Identity, d={np.round(Z_iden_loss, 2)}')
+        ax[2*i+1,3].set_title(f'Identity, d={np.round(z_iden_loss, 2)}')
 
     f.savefig('log_image.png', transparent=True, bbox_inches='tight')
     if plotting == True:
@@ -238,5 +235,5 @@ def visualize_train(config, H2Z, Z2H, d_H, d_Z, x_horse, x_zebra, plotting=False
         plt.show()
     else:
         plt.close()
-    wandb.log({"Train Visualization": wandb.Image("log_image.png")})
+    wandb.log({"Train Visualization": wandb.Image("log_image.png")}, commit=False)
     return None
