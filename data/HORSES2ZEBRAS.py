f154f2bbec9fcd63ead2c0198ef735b3cbbe99ca
Simon Amtoft Pedersen <simon.ipsum@gmail.com>
2021-06-18 11:14:30 +0200

    remove unused imports

--- data/HORSES2ZEBRAS.py
+++ data/HORSES2ZEBRAS.py
@@ -1,17 +1,11 @@
-import os
-import csv
 import glob
 import torch
-import numpy as np
 from PIL import Image
-import torchvision.transforms as transforms
 
 
 class HORSES(torch.utils.data.Dataset):
-  def __init__(self, transform, dataset = "train", data_path="horse2zebra/"):
-    """ dataset = "train","test"
-        
-    """
+  def __init__(self, transform, dataset="train", data_path="horse2zebra/"):
+    """ dataset = "train", "test" """
     self.dataset = dataset
     data_path0 = data_path + dataset + "/A"
     self.image_paths = glob.glob(data_path0 + "/*.jpg")
@@ -27,12 +21,11 @@ class HORSES(torch.utils.data.Dataset):
     image = Image.open(image_path).convert('RGB')
     X = self.transform(image)
     return X
-  
+
+
 class ZEBRAS(torch.utils.data.Dataset):
-  def __init__(self, transform, dataset = "train", data_path="horse2zebra/"):
-    """ dataset = "train","test"
-        
-    """
+  def __init__(self, transform, dataset="train", data_path="horse2zebra/"):
+    """ dataset = "train", "test" """
     self.dataset = dataset
     data_path0 = data_path + dataset + "/B"
     self.image_paths = glob.glob(data_path0 + "/*.jpg")
