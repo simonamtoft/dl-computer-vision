30d7ea4ac0149008eec194c741e836f59db0202e
Marc Bøg <s173905@student.dtu.dk>
2021-06-21 11:20:36 +0200

    Add spectral norm

--- models/GAN.py
+++ models/GAN.py
@@ -1,16 +1,17 @@
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.nn.utils import spectral_norm
 
 norm_layer = nn.InstanceNorm2d
 
 
 class ResBlock(nn.Module):
-    def __init__(self, f):
+    def __init__(self, f, sn):
         super(ResBlock, self).__init__()
         self.conv = nn.Sequential(
-            nn.Conv2d(f, f, 3, 1, 1), 
+            sn(nn.Conv2d(f, f, 3, 1, 1)), 
             norm_layer(f), nn.ReLU(),
-            nn.Conv2d(f, f, 3, 1, 1)
+            sn(nn.Conv2d(f, f, 3, 1, 1))
         )
         self.norm = norm_layer(f)
     def forward(self, x):
@@ -23,29 +24,34 @@ class Generator(nn.Module):
         f = config['n_features']
         blocks = config['n_blocks']
 
+        if "spectral_norm" in config and config["spectral_norm"]:
+            sn = spectral_norm
+        else:
+            sn = lambda x: x
+
         # Encoding layer
         layers = [
             nn.ReflectionPad2d(3),
-            nn.Conv2d(  3,   f, kernel_size=7, stride=1, padding=0), 
+            sn(nn.Conv2d(  3,   f, kernel_size=7, stride=1, padding=0)), 
             norm_layer(  f), nn.ReLU(True),
-            nn.Conv2d(  f, 2*f, kernel_size=3, stride=2, padding=1), 
+            sn(nn.Conv2d(  f, 2*f, kernel_size=3, stride=2, padding=1)), 
             norm_layer(2*f), nn.ReLU(True),
-            nn.Conv2d(2*f, 4*f, kernel_size=3, stride=2, padding=1), 
+            sn(nn.Conv2d(2*f, 4*f, kernel_size=3, stride=2, padding=1)), 
             norm_layer(4*f), nn.ReLU(True)
         ]
 
         # Transformation (Resnet) layer
         for _ in range(int(blocks)):
-            layers.append(ResBlock(4*f))
+            layers.append(ResBlock(4*f, sn))
         
         # Decoding layer
         # Uses a subpixel convolution (PixelShuffle) for upsamling 
         layers.extend([ 
-                nn.ConvTranspose2d(4*f, 4*2*f, kernel_size=3, stride=1, padding=1), 
+                sn(nn.ConvTranspose2d(4*f, 4*2*f, kernel_size=3, stride=1, padding=1)), 
                 nn.PixelShuffle(2), norm_layer(2*f), nn.ReLU(True),
-                nn.ConvTranspose2d(2*f,   4*f, kernel_size=3, stride=1, padding=1), 
+                sn(nn.ConvTranspose2d(2*f,   4*f, kernel_size=3, stride=1, padding=1)), 
                 nn.PixelShuffle(2), norm_layer(  f), nn.ReLU(True),
-                nn.ReflectionPad2d(3), nn.Conv2d(f, 3, 7, 1, 0),
+                nn.ReflectionPad2d(3), sn(nn.Conv2d(f, 3, 7, 1, 0)),
                 nn.Tanh()
         ])
         self.conv = nn.Sequential(*layers)
@@ -60,23 +66,28 @@ class Discriminator(nn.Module):
         f = config['n_features']
         relu_val = config['relu_val']
 
+        if "spectral_norm" in config and config["spectral_norm"]:
+            sn = spectral_norm
+        else:
+            sn = lambda x: x
+
         self.conv = nn.Sequential(
-            nn.Conv2d(  3,   f, kernel_size=4, stride=2, padding=1), 
+            sn(nn.Conv2d(  3,   f, kernel_size=4, stride=2, padding=1)), 
             norm_layer(  f), 
             nn.LeakyReLU(relu_val, True),
-            nn.Conv2d(  f, 2*f, kernel_size=4, stride=2, padding=1), 
+            sn(nn.Conv2d(  f, 2*f, kernel_size=4, stride=2, padding=1)), 
             norm_layer(2*f), 
             nn.LeakyReLU(relu_val, True),
-            nn.Conv2d(2*f, 4*f, kernel_size=4, stride=2, padding=1), 
+            sn(nn.Conv2d(2*f, 4*f, kernel_size=4, stride=2, padding=1)), 
             norm_layer(4*f),
             nn.LeakyReLU(relu_val, True),
-            nn.Conv2d(4*f, 8*f, kernel_size=4, stride=1, padding=1), 
+            sn(nn.Conv2d(4*f, 8*f, kernel_size=4, stride=1, padding=1)), 
             norm_layer(8*f),
             nn.LeakyReLU(relu_val, True),
         )
 
         self.final = nn.Sequential(
-            nn.Conv2d(8*f, 1, kernel_size=4, stride=1, padding=1)
+            sn(nn.Conv2d(8*f, 1, kernel_size=4, stride=1, padding=1))
         )
 
     def forward(self, x):
